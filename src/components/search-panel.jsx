e872ff9e66958c4fb66c092b26dc141a52547bc0
Shatki <Shatki@mail.ru>
2019-05-13 09:51:26 +0300

    структура проекта

---
+++ src/components/search-panel.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const SearchPanel = () => {
+    const searchText = 'Type Here to search';
+    return <input placeholder = {searchText}/>
+};
+
+export default SearchPanel
